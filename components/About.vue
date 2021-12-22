25180152f63ae33fcc9c704f641104d4bcb08040
ianujvarshney <anujkk19111997@gmail.com>
2021-12-22 16:04:16 +0530

    abc commit

---
+++ components/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Hello i'm About.vue</h1>
+</template>
+<script setup lang="js">
+export default{
+    }
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>
+      <ul>
+        <li>
+          <NuxtLink to="./components/Home_1">Home_1</NuxtLink>
+        </li>
+        <li><NuxtLink to="/Home">Home</NuxtLink></li>
+        <li><NuxtLink to="/About">About</NuxtLink></li>
+      </ul>
+    </header>
+  </div>
+</template>
+<script>
+export default {}
+</script>
