5505083c415924b997eff606bef38aa672f4017b
ianujvarshney <anujkk19111997@gmail.com>
2021-12-23 15:56:05 +0530

    intital new commit

--- components/Home_1.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1>Hello i'm Home.vue</h1>
-</template>
-<script setup lang="js">
-export default{
-    }
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,14 @@
 <template>
   <div>
-    <h1>Hello from home components</h1>
-    <!-- <button v-on:click="hello('hello World')">Click Me</button> -->
-    <h1 v-if="show">Click Me if</h1>
-    <h1 v-else>Click Me else</h1>
-    <button v-on:click="display">Click Me</button>
+    <home />
   </div>
 </template>
 <script>
+import home from './Home-Page/Files/Home.vue'
 export default {
-  name: 'Home',
-  props: {
-    data: String,
-    msg: String,
+  components: {
+    home,
   },
-  // methods: {
-  //   hello(string) {
-  //     alert(string)
-  //   },
-  // },
-  data() {
-    return { show: true }
-  },
-  methods: {
-    display() {
-      this.show = !this.show
-    },
-  },
-  // asyncData(context) {
-  //   // if (process.server) {
-  //   //   console.log('hi i  a server')
-  //   // }
-  //   // return { blah: 'h1' }
-  // },
-  // middleware: 'erik',
 }
 </script>
 <style></style>
